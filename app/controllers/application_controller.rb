7338a718c594be7af26c6fc42b9a82bd0096f1a4
David Grilli <djgrill@gmail.com>
2019-11-28 13:13:43 +0900

    implements current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
+  before_action :set_current_user
+
+  private
+
+  def set_current_user
+    @current_user = User.find_or_create_by!(uuid: params[:uuid])
+  end
 end
