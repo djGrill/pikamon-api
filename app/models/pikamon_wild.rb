03bb2dede8f8fc7e7b90f0d0eac5c1bb4860de32
David Grilli <djgrill@gmail.com>
2019-11-28 12:08:23 +0900

    implements PikamonWild.within

--- app/models/pikamon_wild.rb
+++ app/models/pikamon_wild.rb
@@ -6,5 +6,21 @@ class PikamonWild < ApplicationRecord
   belongs_to :pikamon
   belongs_to :user
 
-  validates :pikamon, :user, :hp, :cp, :lonlat, presence: true
+  validates :pikamon, :hp, :cp, :lonlat, presence: true
+
+  scope :within, lambda { |lat, lon, distance_in_meters = 5|
+    where_format = format(%{
+     ST_DWithin(lonlat,
+                ST_GeomFromText('POINT(%f %f)', 4326)::geography,
+                %d,
+                true)
+    }, lon, lat, distance_in_meters)
+
+    order_format = format(%{
+     ST_Distance(lonlat, 'POINT(%f %f)')
+    }, lon, lat)
+
+    where(Arel.sql(where_format))
+      .order(Arel.sql(order_format))
+  }
 end
